4a3a25132c96448836b75b4013f564eb332b72f1
Diogo Merelles <diogo.merelles.faria@gmail.com>
2022-11-18 13:03:01 +0000

    form validation in react added

---
+++ src/Containers/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {useNavigate} from 'react-router-dom';
+
+import './Home.css';
+
+const Home = () => {
+
+    const navigate = useNavigate();
+
+    return(
+        <div className='homeDesign'>
+            <div className='linkDesign' onClick={()=>setTimeout(()=>{navigate('/login')},333)}>Login</div>
+            <div className='linkDesign' onClick={()=>setTimeout(()=>{navigate('/register')},333)}>Register</div>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/Containers/Register/Register.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegisterCard from '../../Components/RegisterCard'
+import "./Register.css";
+
+const Register = () => {
+
+    return (
+        <div className='registerDesign'>
+            <RegisterCard />
+        </div>
+    )
+}
+
+export default Register;
